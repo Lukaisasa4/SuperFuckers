ef0ce1f29beff27dd0abbcdcd2adb4383e2f4d60
Oihantx <161827610+Oihantx@users.noreply.github.com>
2025-05-27 09:13:33 +0200

    bbdd comentauu

--- backend/run_once.py
+++ backend/run_once.py
@@ -1,4 +1,5 @@
 from database import engine
 from models import Base
 
+#Esta línea crea todas las tablas definidas con las clases ORM (Location, Weather, etc.) en la base de datos, si no existen aún.
 Base.metadata.create_all(bind=engine)
